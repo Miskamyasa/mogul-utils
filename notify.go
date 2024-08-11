4f4635b68383f9999adc0e59325239dd0267cdec
Denis Zaitsev <zaytsevdi@gmail.com>
2024-08-11 18:11:49 +0300

    stdlib logger

--- notify.go
+++ notify.go
@@ -1,13 +1,13 @@
 package utils
 
 import (
-	"github.com/gofiber/fiber/v2/log"
+	"log"
 	"os"
 )
 
 func Notify(msg string, err error) {
 	// TODO: send admin notification
-	log.Debugf("%s: %v", msg, err)
+	log.Printf("%s: %v", msg, err)
 }
 
 func Fatal(msg string, err error) {
