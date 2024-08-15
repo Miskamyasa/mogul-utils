be9a4f12955ca8738339208a2b060b73ba0488a3
Denis Zaitsev <zaytsevdi@gmail.com>
2024-08-15 23:00:54 +0300

    removed rate limiter

--- middlewares.go
+++ middlewares.go
@@ -1,11 +1,7 @@
 package utils
 
 import (
-	"os"
-	"time"
-
 	"github.com/gofiber/fiber/v2"
-	"github.com/gofiber/fiber/v2/middleware/limiter"
 )
 
 func GenerateCacheKey(req *fiber.Ctx) string {
@@ -19,27 +15,4 @@ func CacheMiddleware(req *fiber.Ctx) error {
 		return req.JSON(payload)
 	}
 	return req.Next()
-}
-
-func RateLimiterMiddleware() fiber.Handler {
-	var Next func(req *fiber.Ctx) bool
-
-	if os.Getenv("ENV") == "development" {
-		Next = func(req *fiber.Ctx) bool {
-			return req.IP() == "127.0.0.1"
-		}
-	} else {
-		Next = limiter.ConfigDefault.Next
-	}
-
-	limiterTTL, err := time.ParseDuration(os.Getenv("RATE_LIMITER_TTL"))
-	if err != nil {
-		limiterTTL = 5 * time.Minute
-	}
-
-	return limiter.New(limiter.Config{
-		Next:       Next,
-		Max:        1,
-		Expiration: limiterTTL,
-	})
 }
