447e782b2ffa9bc4b8055354575781efa328600f
Denis Zaitsev <zaytsevdi@gmail.com>
2024-08-23 21:30:42 +0300

    mongo

---
+++ mongo.go
@@ -0,0 +1,32 @@
+package utils
+
+import (
+	"context"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"log"
+	"os"
+	"time"
+)
+
+var (
+	mongoDB *mongo.Database
+)
+
+func InitMongoDB() *mongo.Database {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(os.Getenv("MONGODB_URL")))
+	if err != nil {
+		log.Fatalf("Failed to connect to MongoDB: %v", err)
+	}
+
+	err = client.Ping(ctx, nil)
+	if err != nil {
+		log.Fatalf("Failed to ping MongoDB: %v", err)
+	}
+
+	mongoDB = client.Database(os.Getenv("MONGODB_NAME"))
+	return mongoDB
+}
